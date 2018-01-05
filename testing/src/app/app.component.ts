42b9bd52ec2e4853117604bd6166cc909f939d2a
pvink <pvink@vertexinc.com>
2018-01-05 14:29:18 -0500

    Added test for notifications component

--- testing/src/app/app.component.ts
+++ testing/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NotificationsService} from 'angular2-notifications/dist';
 
 @Component({
   selector: 'app-root',
@@ -58,5 +59,22 @@ export class AppComponent {
     'star-filled',
     'star-hollow'
   ];
+
+  options = {
+    animate: 'fromBottom',
+    timeOut: 10000,
+    showProgressBar: true,
+    pauseOnHover: true,
+    lastOnBottom: false
+  };
+
+  myTitle = 'Sample Message';
+
+  message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam bibendum, justo in gravida ultricies, ' +
+    'odio enim rutrum erat, id rutrum libero libero sed libero. Sed et ligula ac justo fringilla tempus ac non ' +
+    'sapien. In congue sodales diam, porta aliquet lectus pulvinar eu. In sed tellus eu felis ornare tempus. ' +
+    'Suspendisse at lacus nec magna maximus lobortis in non quam.';
+
+  public notificationsService: NotificationsService = new NotificationsService;
 
 }
